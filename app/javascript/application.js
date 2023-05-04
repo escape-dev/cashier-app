a15c325a2f988a5562baca36b3caadbf76d674c0
Muhammad Hafiz <hafiz170301@gmail.com>
2023-05-04 18:40:30 +0700

    change theme

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require js/core/popper.min.js
+//= require js/core/bootstrap.min.js
+//= require js/plugins/perfect-scrollbar.min.js
+//= require js/plugins/smooth-scrollbar.min.js
+//= require js/material-dashboard.min.js
+//= require js/plugins/chartjs.min.js
+//= require js/plugins/Chart.extension.js
+//= require js/material-dashboard.min.js
